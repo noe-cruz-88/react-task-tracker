009cf95b64eb8b9708cde6d70a2ee71a5c6527e0
Noé Cruz <educacion.mcon88@gmail.com>
2023-11-23 16:51:09 -0600

    featt(sumbit): implement submit logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
     const [tasks, setTasks] = useState([
@@ -24,6 +25,13 @@ function App() {
         },
     ])
 
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([...tasks, newTask])
+    }
+
     // Delete Task
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id != id))
@@ -42,6 +50,7 @@ function App() {
         <>
             <div className="container">
                 <Header />
+                <AddTask onAdd={addTask} />
                 {tasks.length > 0 ? (
                     <Tasks
                         tasks={tasks}
